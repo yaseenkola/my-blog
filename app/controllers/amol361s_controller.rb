fc25472293649fc4eb1d8290967b0aca1ea4890a
Yaseen Kola <yaseenkola@gmail.com>
2019-05-30 09:22:24 +0000

    Initail Commit

--- app/controllers/amol361s_controller.rb
+++ app/controllers/amol361s_controller.rb
@@ -5,7 +5,7 @@ class Amol361sController < ApplicationController
   before_action :set_amol361, only: [:show, :edit, :update, :destroy]
   
   def index
-    @amol361s = current_user.amol361s.search(params[:search]).visible
+    @amol361s = current_user.amol361s.all.search(params[:search]).visible
     # @amol361s = current_user.amol361s.all.visible
     # @amol361s = Amol361.search(params[:search]).visible
 		respond_to do |format|
