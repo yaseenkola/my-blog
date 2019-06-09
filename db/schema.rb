bf1492c7f16804e5ad92935b5eda9081e7d4c78a
Yaseen Kola <yaseenkola@gmail.com>
2019-06-09 13:03:47 +0000

    Initail Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190602072409) do
+ActiveRecord::Schema.define(version: 20190609125447) do
 
   create_table "amol361s", force: :cascade do |t|
     t.date "date"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20190602072409) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "username"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
